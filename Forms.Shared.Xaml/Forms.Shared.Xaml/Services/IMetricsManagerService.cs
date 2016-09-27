7d90f9e3c110135d9c78ece492abe730f1af583f
Paul DeCarlo <pdecarlo@microsoft.com>
2016-09-26 19:44:38 -0500

    Add IMetricsManagerService Interface to Shared Project

---
+++ Forms.Shared.Xaml/Forms.Shared.Xaml/Services/IMetricsManagerService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forms.Shared.Xaml.Services
+{
+    public interface IMetricsManagerService
+    {
+        void TrackEvent(string eventName);
+    }
+}
